f1592cbe64754d17a2c0f63281edc406ec5df19a
Melissa <melissakalish2@gmail.com>
2023-07-23 16:44:33 -0500

    fixes and work progress

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,13 @@
+//Create a License Badge 
+const licenseDetails = [
+    {
+    name: "Apache license 2.0",
+    babdge: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    link: "https://opensource.org/licenses/Apache-2.0"
+}
+]
+module.exports = licenseDetails
+
 
 /// Create license link
 function renderLicenseLink(license) {
